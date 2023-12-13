267040bc15ab576d2b35f37f952f0072bb194a42
junyeong-0825 <scv9761@gmail.com>
2023-12-13 15:12:12 +0900

    [add] Btn
    입금, 출금 메뉴 열리도록 버튼 설정
    뒤로가기를 누를 시 처음에 보여주는 입금, 출금창으로 가는 기능 추가

--- Assets/Scripts/BankSystem.cs
+++ Assets/Scripts/BankSystem.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BankSystem : MonoBehaviour
 {
     public TMP_Text cash;
     public TMP_Text balance;
-
+    public GameObject TitleBtn;
+    public GameObject DepositBtn;
+    public GameObject WithdrawalBtn;
     private int haveCash = 100000;
     private int haveBalance = 50000;
 
@@ -15,6 +18,34 @@ public class BankSystem : MonoBehaviour
     {
         cash.text = haveCash.ToString();
         balance.text = haveBalance.ToString();
+    }
+
+    public void CallDepositWindow()
+    {
+        TitleBtn.SetActive(false);
+        DepositBtn.SetActive(true);
+    }
+    public void CallWithdrawalWindow()
+    {
+        TitleBtn.SetActive(false);
+        WithdrawalBtn.SetActive(true);
+    }
+    public void BackSpaceBtn()
+    {
+        TitleBtn.SetActive(true);
+        WithdrawalBtn.SetActive(false);
+        DepositBtn.SetActive(false);
+    }
+
+    void Deposit(int value)
+    {
+        haveCash -= value;
+        haveBalance += value;
+    }
+    void Withdrawal(int value)
+    {
+        haveBalance -= value;
+        haveCash += value;
     }
 
 
